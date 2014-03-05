734b1d579ecdbe77dd688c3099efc55b684dec20
Logan Hasson <logan.hasson@gmail.com>
2014-03-05 15:01:31 -0500

    Write pending GenresController specs

--- spec/controllers/genres_controller_spec.rb
+++ spec/controllers/genres_controller_spec.rb
@@ -1,4 +1,69 @@
 require 'spec_helper'
 
 describe "GenresController" do
+  describe "GET /genres" do
+    before do
+      # Setup code here.
+    end
+
+    it "responds with a 200 status code" do
+      pending
+    end
+
+    it "renders the genres index template" do
+      pending
+    end
+  end
+
+  describe "GET /genres/new" do
+    before do
+      # Setup code here.
+    end
+
+    it "responds with a 200 status code" do
+      pending
+    end
+
+    it "renders the new genre form" do
+      pending
+    end
+  end
+
+  describe "POST /genres" do
+    before do
+      # Setup code here.
+    end
+
+    it "redirects to the genres index page" do
+      pending
+    end
+  end
+
+  describe "GET /genres/:id/actors" do
+    before do
+      # Setup code here.
+    end
+
+    it "responds with a 200 status code" do
+      pending
+    end
+
+    it "renders the show page for the genre's list of actors" do
+      pending
+    end
+  end
+
+  describe "GET /genres/:id/movies" do
+    before do
+      # Setup code here.
+    end
+
+    it "responds with a 200 status code" do
+      pending
+    end
+
+    it "renders the show page for the genre's list of movies" do
+      pending
+    end
+  end
 end
