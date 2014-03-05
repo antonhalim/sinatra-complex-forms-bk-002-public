c9c455e97b478b5d035533153faaaa263b50c368
Logan Hasson <logan.hasson@gmail.com>
2014-03-05 15:00:05 -0500

    Write pending ActorsController specs

--- spec/controllers/actors_controller_spec.rb
+++ spec/controllers/actors_controller_spec.rb
@@ -1,4 +1,45 @@
 require 'spec_helper'
 
 describe "ActorsController" do
+  describe "GET /actors" do
+    before do
+      # Setup code here.
+    end
+
+    it "responds with a 200 status code" do
+      pending
+    end
+
+    it "renders the actors index template" do
+      pending
+    end
+  end
+
+  describe "GET /actors/:id/movies" do
+    before do
+      # Setup code here.
+    end
+
+    it "responds with a 200 status code" do
+      pending
+    end
+
+    it "renders the show page for the actor's list of movies" do
+      pending
+    end
+  end
+
+  describe "GET /actors/:id/genres" do
+    before do
+      # Setup code here.
+    end
+
+    it "responds with a 200 status code" do
+      pending
+    end
+
+    it "renders the show page for the actor's list of genres" do
+      pending
+    end
+  end
 end
